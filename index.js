c083109bc50be793a8f9fc0ca96168178f57915e
zubair-appiskey <zubair.moosani@appiskey.com>
2023-01-15 19:31:10 +0500

    create typescript todo list cli app wtih inquirer

---
+++ index.js
@@ -0,0 +1,29 @@
+import inquirer from "inquirer";
+let todos = [];
+let isAddMore = true;
+while (isAddMore) {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "todo",
+            message: "Add your new todo!",
+        },
+        {
+            type: "confirm",
+            name: "addMore",
+            message: "Do you want add more todo's?",
+        },
+    ]);
+    const { todo, addMore } = answers;
+    isAddMore = addMore;
+    if (todo) {
+        todos.push(todo);
+    }
+    else {
+        console.log("Invalid input");
+    }
+}
+console.log("Todo list:\n");
+todos.map((todo, index) => {
+    console.log(`${++index}: ${todo}`);
+});
---
+++ index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+
+let todos: string[] = [];
+let isAddMore: boolean = true;
+
+type Answers = {
+  todo: string;
+  addMore: boolean;
+};
+while (isAddMore) {
+  const answers: {
+    todo: string;
+    addMore: boolean;
+  } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "todo",
+      message: "Add your new todo!",
+    },
+    {
+      type: "confirm",
+      name: "addMore",
+      message: "Do you want add more todo's?",
+    },
+  ]);
+
+  const { todo, addMore } = answers;
+  isAddMore = addMore;
+
+  if (todo) {
+    todos.push(todo);
+  } else {
+    console.log("Invalid input");
+  }
+}
+console.log("Todo list:\n");
+todos.map((todo, index) => {
+  console.log(`${++index}: ${todo}`);
+});
